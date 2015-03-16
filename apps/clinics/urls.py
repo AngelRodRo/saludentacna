67a7acfef264e64ecc13717cd91327a72b0b427f
angel <aleangrodriguez321@hotmail.com>
2015-03-16 16:54:34 -0500

    Añadido vista para busqueda de lugares

--- apps/clinics/urls.py
+++ apps/clinics/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import patterns,  url
 
-from .views import ClinicList
+from .views import ClinicList, ClinicSearch
 
 urlpatterns = patterns('',
 
     # url(r'^$', clinicindex.as_view(),name='index'),
     # url(r'^add/$', RegisterClinic.as_view(),name='clinic_add'),
     url(r'^$', ClinicList.as_view(),name='clinic_list'),
+    url(r'^search/$', ClinicSearch.as_view(),name='clinic_search')
     # url(r'^(?P<pk>\d+)/$', ClinicaDetailView.as_view(),name='clinic_detail'),
 )
--- apps/clinics/views.py
+++ apps/clinics/views.py
@@ -5,3 +5,7 @@ from .models import Clinic
 class ClinicList(ListView):
     template_name = 'clinicas/lista_clinicas.html'
     queryset = Clinic.objects.all()
+
+class ClinicSearch(ListView):
+    template_name = 'clinicas/buscar_clinicas.html'
+    queryset = Clinic.objects.all()
